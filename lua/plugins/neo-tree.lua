f4bd6cc2a2049518ac84dbcb4523ca7a92429f83
dips <dipeshshrestha910@gmail.com>
2024-07-16 14:48:27 +0545

    changed color theme to tookyonights

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,41 +1,44 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		-- Key mappings
-		  vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    -- Key mappings
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
 
-		-- Setup NeoTree
-		require("neo-tree").setup({
-			event_handlers = {
-				{
-					event = "file_opened",
-					handler = function(file_path)
-						require("neo-tree.command").execute({ action = "close" })
-					end,
-				},
-			},
-			mappings = {
-				["A"] = "add_directory",
-				["d"] = "delete",
-				["r"] = "rename",
-				["y"] = "copy_to_clipboard",
-				["x"] = "cut_to_clipboard",
-				["p"] = "paste_from_clipboard",
-				["c"] = "copy", -- You can also configure this with optional options
-				["m"] = "move", -- You can also configure this with optional options
-				["q"] = "close_window",
-				["R"] = "refresh",
-				["?"] = "show_help",
-				["<"] = "prev_source",
-				[">"] = "next_source",
-				["i"] = "show_file_details",
-			},
-		})
-	end,
+    -- Setup NeoTree
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+      mappings = {
+        ["A"] = "add_directory",
+        ["d"] = "delete",
+        ["r"] = "rename",
+        ["y"] = "copy_to_clipboard",
+        ["x"] = "cut_to_clipboard",
+        ["p"] = "paste_from_clipboard",
+        ["c"] = "copy", -- You can also configure this with optional options
+        ["m"] = "move", -- You can also configure this with optional options
+        ["q"] = "close_window",
+        ["R"] = "refresh",
+        ["?"] = "show_help",
+        ["<"] = "prev_source",
+        [">"] = "next_source",
+        ["i"] = "show_file_details",
+      },
+    })
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
+  end,
 }
