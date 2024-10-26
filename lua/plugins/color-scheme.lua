136e9ceed31b9a2c36f4f86ea977e6f5858df7fa
dips <dipeshshrestha910@gmail.com>
2024-10-26 22:55:23 +0545

    added tab

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -18,7 +18,7 @@
 	},
 } ]]
 
---[[ return {
+--[[ return false{
 	{
 		"craftzdog/solarized-osaka.nvim",
 		lazy = false,
@@ -30,41 +30,40 @@
 	},
 } ]]
 
--- ~/.config/nvim/lua/plugins/kanagawa.lua
---   return {
---   'rebelot/kanagawa.nvim',
---   config = function()
---     require('kanagawa').setup({
--- require('kanagawa').setup({
---     compile = false,             -- enable compiling the colorscheme
---     undercurl = true,            -- enable undercurls
---     commentStyle = { italic = true },
---     functionStyle = {},
---     keywordStyle = { italic = true},
---     statementStyle = { bold = true },
---     typeStyle = {},
---     transparent = false,         -- do not set background color
---     dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
---     terminalColors = true,       -- define vim.g.terminal_color_{0,17}
---     colors = {                   -- add/modify theme and palette colors
---         palette = {},
---         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
---     },
---     overrides = function(colors) -- add/modify highlights
---         return {}
---     end,
---     theme = "dragon",              -- Load "wave" theme when 'background' option is not set
---     background = {               -- map the value of 'background' option to a theme
---         dark = "dragon",           -- try "dragon" !
---         light = "lotus"
---     },
--- })
---
---      })
---     -- setup must be called before loading
---     vim.cmd("colorscheme kanagawa")
---   end
--- } 
+return {
+  'rebelot/kanagawa.nvim',
+  config = function()
+    require('kanagawa').setup({
+      require('kanagawa').setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = true, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {         -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "dragon", -- Load "wave" theme when 'background' option is not set
+        background = {   -- map the value of 'background' option to a theme
+          dark = "dragon", -- try "dragon" !
+          light = "lotus"
+        },
+      })
+
+    })
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa")
+  end
+}
 
 --
 --    return {
@@ -86,7 +85,7 @@
 --   },
 --
 
-  return {
+--[[   return {
 	"navarasu/onedark.nvim",
 	config = function()
 		-- Lua
@@ -132,4 +131,4 @@
     vim.cmd.colorscheme("onedark")
 	end,
 
-}
+} ]]
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,17 +9,19 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.breakindent=true
 
-vim.opt.wrap =false 
-vim.opt.laststatus=0
+vim.opt.wrap = false
+vim.opt.laststatus = 0
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = vim.env.HOME .. "/.vim/undodir" -- Use vim.env instead of os.getenv
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 --for navigating vim panes
@@ -29,8 +31,8 @@ vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>w", ":update<Return>", { silent = true }) -- Saves the file if changed
-vim.keymap.set("n", "<leader>q", ":quit<Return>", { silent = true }) -- Quits the current window
-vim.keymap.set("n", "<leader>Q", ":qa<Return>", { silent = true }) -- Quits all windows
+vim.keymap.set("n", "<leader>q", ":quit<Return>", { silent = true })   -- Quits the current window
+vim.keymap.set("n", "<leader>Q", ":qa<Return>", { silent = true })     -- Quits all windows
 vim.opt.updatetime = 50
 
 vim.keymap.set("n", "<A-l>", "5zl")
