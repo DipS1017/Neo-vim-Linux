3ad20b50a188d444cd0d0dcaaac98d7eeca0404a
dips <dipeshshrestha910@gmail.com>
2025-01-10 15:07:45 +0545

    added a diagnostic on K instead of hover

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -62,10 +62,13 @@ lspconfig.omnisharp.setup({
           less = { validate = true },
         },
       })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+-- Show diagnostics in a floating window
+vim.keymap.set("n", "K", vim.diagnostic.open_float, { desc = "Show diagnostics" })
     end,
   },
 }
