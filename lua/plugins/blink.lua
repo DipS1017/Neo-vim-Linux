93660c34e956084a83cd76b6f33b017b5a84d749
dips <dipeshshrestha910@gmail.com>
2025-01-10 15:44:45 +0545

    added copilot finished setting up finally

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,6 @@ return{
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = 'rafamadriz/friendly-snippets',
-  
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -18,7 +17,7 @@ return{
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'enter' },
+    keymap = { preset = 'default' },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
