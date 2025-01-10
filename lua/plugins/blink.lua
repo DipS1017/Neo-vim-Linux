7ce05f51f01680784cbc1828b5e52582ad46f533
dips <dipeshshrestha910@gmail.com>
2025-01-10 14:45:31 +0545

    added blink settigns to be liek vscode

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,7 @@ return{
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = 'rafamadriz/friendly-snippets',
+  
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -17,7 +18,7 @@ return{
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
+    keymap = { preset = 'super-tab' },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
